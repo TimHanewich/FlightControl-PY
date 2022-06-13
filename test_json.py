0736d1ce7e9074663cc32b7951e269faac3c721b
Tim Hanewich <tahanewich@live.com>
2022-06-13 18:08:30 -0400

    Added rythm driver

--- test_json.py
+++ test_json.py
@@ -1,5 +1,6 @@
 import json
 import rythm
+import RPi.GPIO
 
 f = open(r"C:\Users\tahan\Downloads\FlightControl-PY\j.json", "r")
 x = f.read()
@@ -10,3 +11,16 @@ rm = rythm.rythm_machine(y)
 lopis = rm.calc_lopi(1)
 for l in lopis:
     print(str(l.status) + " " + str(l.duration))
+
+
+
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BOARD)
+rd = rythm.rythm_driver(11)
+print("Ready to execute!")
+input("Press enter")
+rd.execute(lopis)
+
+
+print("Done")
+GPIO.cleanup()
