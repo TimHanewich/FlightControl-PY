1cd5a395932dc5b02e703db536491d4792d3e095
Tim Hanewich <tahanewich@live.com>
2022-06-13 22:42:31 -0400

    Removed lopi

--- rythm.py
+++ rythm.py
@@ -10,11 +10,6 @@ class note:
     start = 0.0 #what beat to start on
     duration = 0.25 #duration in beats
 
-# "Light operation instruction" - calculated BASED ON the notes above
-class lopi:
-    status = False # False = light off, True = light on
-    duration = 0.0 #duration to hold, in seconds
-
 
 class rythm_machine:
     __beatsec__:float = 0.0
@@ -51,45 +46,6 @@ class rythm_machine:
         toreturn = beat * self.__beatsec__
         return toreturn
 
-    # construction of the LOPI for a particular ID
-    def calc_lopi(self, id:int):
-        idnotes = self.notes_for_id(id)
-        idnotes = sort_notes(idnotes)
-        toreturn = []
-
-        # If there is an offset (period to wait before starting), add an empty time now
-        if (self.offset > 0):
-            op = lopi()
-            op.status = False
-            op.duration = self.offset
-            toreturn.append(op)
-        
-        # If the first note (it will be arranged) does not occur on beat 0 (the beginning), create the empty space at first
-        if idnotes[0].start != 0:
-            eb = lopi()
-            eb.status = False
-            eb.duration = self.beats_to_seconds(idnotes[0].start)
-            toreturn.append(eb)
-        
-        last_note = None
-        for n in idnotes:
-
-            # if last_note is NOT null, it means we need to inject an empty period here
-            if last_note != None:
-                tlopi = lopi()
-                tlopi.status = False
-                tlopi.duration = self.beats_to_seconds(n.start - last_note.start)
-                toreturn.append(tlopi)
-            
-            nlopi = lopi()
-            nlopi.status = True
-            nlopi.duration = self.beats_to_seconds(n.duration)
-            toreturn.append(nlopi)
-
-            # mark the last note, so that way it will calculate the empty area on the next loop
-            last_note = n
-        
-        return toreturn
 
 
             
