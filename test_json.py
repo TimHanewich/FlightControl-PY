88032fba4585aeffb9e043a33843a83c30b80e76
Tim Hanewich <tahanewich@live.com>
2022-06-13 23:45:15 -0400

    Made GPIO test

--- test_json.py
+++ test_json.py
@@ -3,6 +3,8 @@ import rythm
 import settings
 import threading
 import resources
+import RPi.GPIO as GPIO
+import time
 
 f = open(settings.song1, "r")
 x = f.read()
@@ -10,17 +12,31 @@ y = json.loads(x)
 
 rm = rythm.rythm_machine(y)
 
-v = rm.to_gpioits()
-v = resources.sort_gpioits(v)
-for vv in v:
-    print(str(vv.pin) + " " + str(vv.status) + " " + str(vv.time))
 
-print("---")
-print("---")
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+
+
 
 gpiois = rm.to_gpiois()
 for g in gpiois:
     if type(g) == float:
         print("Wait " + str(g) + " seconds")
+        time.sleep(g)
     else:
         print("Pin " + str(g.pin) + ": " + str(g.status))
+        if g.pin == 0 and g.status == True:
+            GPIO.output(11, GPIO.HIGH)
+        elif g.pin == 1 and g.status == True:
+            GPIO.output(13, GPIO.HIGH)
+        if g.pin == 0 and g.status == False:
+            GPIO.output(11, GPIO.LOW)
+        elif g.pin == 1 and g.status == False:
+            GPIO.output(13, GPIO.LOW)
+        
+        
+
+print("DONE!")
+GPIO.cleanup()
