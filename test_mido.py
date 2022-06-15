2eb70f0f48ce67597cdc3461c04de128774feccd
Tim Hanewich <tahanewich@live.com>
2022-06-15 08:05:42 -0400

    Tested conversion of midi into instructions

--- test_mido.py
+++ test_mido.py
@@ -1,14 +1,23 @@
 from mido import MidiFile
 import mido
 import rythm_midi
+import resources
 
 mid = MidiFile(r"C:\Users\tahan\Downloads\beautiful_people.mid")
 
 rm = rythm_midi.midi_to_rm(mid)
 
 for n in rm.notes:
-    try:
-        print(n.id + " Start: " + str(n.start) + " Duration: " + str(n.duration))
-    except:
-        print("error")
-    
+    print(n.id + " Start: " + str(n.start) + " Duration: " + str(n.duration))
+
+instructions = rm.to_idis()
+
+map = {"808 Kick": 11, "808 HiHat": 13}
+
+resources.map_id_to_pin(instructions, map)
+
+for i in instructions:
+    if type(i) == float:
+        print("WAIT " + str(i))
+    else:
+        print("ID: " + str(i.id) + " Status: " + str(i.status))
