6a93069cd0eac498656576ff17b93094c52616f2
Tim Hanewich <tahanewich@live.com>
2022-07-02 12:53:09 -0400

    Make it global in each function

--- src/flight_controller.py
+++ src/flight_controller.py
@@ -9,6 +9,7 @@ def set_mean_power(value:float):
 
 # hold X + Z position (all motor power the same)
 def hold():
+    global MEAN_POWER
     motor_driver.set_motor_power(motor_driver.Motor.FrontLeft, MEAN_POWER)
     motor_driver.set_motor_power(motor_driver.Motor.FrontRight, MEAN_POWER)
     motor_driver.set_motor_power(motor_driver.Motor.RearLeft, MEAN_POWER)
@@ -17,6 +18,9 @@ def hold():
 # provide percent as a number. i.e. 0-100. Not 0.0-1.0
 def forward(percent:float):
     adj = settings.slip_diff * (percent/100) #the adjustment (slip) to apply to the motors. rear motors will decrease by this amount from the mean. front motors will increase by this amount
+    
+    # declare var as global first
+    global MEAN_POWER
     
     #set Rears
     motor_driver.set_motor_power(motor_driver.Motor.RearLeft, MEAN_POWER - adj)
