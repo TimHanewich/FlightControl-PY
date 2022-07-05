2dec4e33257e289ba3350f8c986b5aa80762b8ae
Tim Hanewich <tahanewich@live.com>
2022-07-05 13:22:08 -0400

    Changed to gpio labels

--- src/radio_comm.py
+++ src/radio_comm.py
@@ -3,7 +3,7 @@ from rpi_rf import RFDevice
 
 # send the terminator
 def send_terminator():
-    rf = RFDevice(settings.pin_transmitter)
+    rf = RFDevice(settings.gpio_transmitter)
     rf.enable_tx()
     rf.tx_repeat = 10
     rf.tx_code(settings.rf_terminator, 1, 350, 24)
