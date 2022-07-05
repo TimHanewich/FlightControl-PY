9c52b8a80a3a9735599fa8a2b27f2e453dd09fba
Tim Hanewich <tahanewich@live.com>
2022-07-05 13:35:11 -0400

    Changed to send_code

--- src/radio_comm.py
+++ src/radio_comm.py
@@ -1,13 +1,18 @@
 import settings
 from rpi_rf import RFDevice
 
-# send the terminator
-def send_terminator():
+
+def send_code(code:int):
     rf = RFDevice(settings.gpio_transmitter)
     rf.enable_tx()
     rf.tx_repeat = 10
+
+    # send the code
+    rf.tx_code(code, 1, 350, 24)
+
+    # send the terminator
     rf.tx_code(settings.rf_terminator, 1, 350, 24)
+
     rf.cleanup()
     print("Done!")
 
-send_terminator()
